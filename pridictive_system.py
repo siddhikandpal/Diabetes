de2fb906540774ffabaadf5ff579a53a3793e630
siddhi kandpal <siddhikandpal43@gmail.com>
2024-12-02 08:24:41 +0530

    File Succesfully Uploaded

---
+++ pridictive_system.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+import numpy as np
+import pickle
+
+# loading the saved model
+loaded_model = pickle.load(open("C:/Users\prach\Downloads\Diabetes\Diabetes_web_app.py", 'rb'))
+
+
+input_data = (5,166,72,19,175,25.8,0.587,51)
+
+# changing the input_data to numpy array
+input_data_as_numpy_array = np.asarray(input_data)
+
+# reshape the array as we are predicting for one instance
+input_data_reshaped = input_data_as_numpy_array.reshape(1,-1)
+
+prediction = loaded_model.predict(input_data_reshaped)
+print(prediction)
+
+if (prediction[0] == 0):
+  print('The person is not diabetic')
+elif(prediction[0]==1):
+  print('The person is diabetic')
+else:
+  print('error')
